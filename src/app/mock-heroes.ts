4902407d6e398299b875aaec5bb7a242f3120c4e
Prabhat <prablemo@outlook.com>
2022-08-17 13:48:10 +0545

    learning phase

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Dr. Strange' },
+  { id: 2, name: 'Spiderman' },
+  { id: 3, name: 'Thor' },
+  { id: 4, name: 'IronMan' },
+  { id: 5, name: 'HawkEye' },
+  { id: 6, name: 'Hulk' },
+  { id: 7, name: 'panther' },
+  { id: 8, name: 'Captain America' },
+  { id: 9, name: 'AntMan' }
+];
